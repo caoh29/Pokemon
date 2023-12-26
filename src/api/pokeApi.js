2d705950a30ccc72e1a34b2f70f947cdcf45a806
Camilo Ordoñez <cronox20@gmail.com>
2023-12-26 08:09:02 -0500

    converting to clientside app instead of SSR

--- src/api/pokeApi.js
+++ src/api/pokeApi.js
@@ -1,6 +1,7 @@
 async function fetchPokemons() {
 
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`);
+    // const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`);
+    const response = await fetch(`https://pokemon-backend-dg1j.onrender.com/api/pokemon`);
     return await response.json();
     
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,79 @@
 "use client";
 
+import { useState, useEffect } from "react";
+
 import fetchPokemons from "@/api/pokeApi"
 import PokemonCard from "@/components/PokemonCard/PokemonCard";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { capitalizeFirstLetter } from "@/lib/index";
+
 
 export default function Home() {
-
+	const [isLoading, setIsLoading] = useState(true);
 	const [pokemonsList, setPokemonsList] = useState([]);
 
 	useEffect(() => {
 		const updatePokemons = async () => {
 			const data = await fetchPokemons();
-			const pokemonsList = data.results.map((pokemon, index) => ({
+			const pokemons = data.map((pokemon) => ({
 				...pokemon,
-				id: index + 1,
-				image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg`
+				//_id: index + 1,
+				//image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg`
+				image: pokemon.sprites.other.dream_world.front_default,
 			}));
-			setPokemonsList(pokemonsList);
+			setPokemonsList(pokemons);
+			setIsLoading(false);
 		}
 		updatePokemons();
-	}, [])
+	}, [pokemonsList])
 
-	function capitalizeFirstLetter(string) {
-		return string.charAt(0).toUpperCase() + string.slice(1);
-	}
 
+	if (isLoading) return (
+		<div className="flex flex-col justify-center items-center h-screen">
+			<h1 className="text-xl">Loading...</h1>
+		</div>
+	);
 
 	return (
 		<div className="flex flex-wrap items-center justify-items-center justify-center min-h-screen py-2">
 			{pokemonsList.map((pokemon) => (
-				<PokemonCard key={pokemon.id} pokemonName={pokemon.name} clickable={true}>
-					<h1 className="text-xl text-center font-bold my-2">#{pokemon.id} - {capitalizeFirstLetter(pokemon.name)}</h1>
+				<PokemonCard key={pokemon._id} pokemonName={pokemon.name} clickable={true}>
+					<h1 className="text-xl text-center font-bold my-2">#{pokemon.number} - {capitalizeFirstLetter(pokemon.name)}</h1>
 					<Image src={pokemon.image} width={0} height={0} alt={pokemon.name} className="w-44 h-44"/>
 				</PokemonCard>
 			))}
 		</div>
 	)
 }
+
+// THIS IMPLEMENTATION IS TO USE REACT SERVER COMPONENTS
+// WHAT THIS DOES IS THAT IT FETCHES THE DATA FROM THE API ON THE SERVER DURING BUILD TIME
+// IF SOMETHING CHANGES IN THE DB IT IS NOT CHANGING IN THE SERVER, 
+// SOMEHOW WE NEED TO REVALIDATE THE DATA AFTER A CHANGE OCCURS IN THE DB BECAUSE THE USER COULD BE RECEIVING OUTDATED HTML
+
+// import fetchPokemons from "@/api/pokeApi"
+// import PokemonCard from "@/components/PokemonCard/PokemonCard";
+// import { capitalizeFirstLetter } from "@/lib/index";
+// import Image from "next/image";
+
+// export default async function Home() {
+
+// 	const data = await fetchPokemons();
+// 	const pokemonsList = data.map((pokemon, index) => ({
+// 		...pokemon,
+// 		// _id: index + 1,
+// 		// image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg`
+// 		image: pokemon.sprites.other.dream_world.front_default,
+// 	}));
+
+// 	return (
+// 		<div className="flex flex-wrap items-center justify-items-center justify-center min-h-screen py-2">
+// 			{pokemonsList.map((pokemon) => (
+// 				<PokemonCard key={pokemon._id} pokemonName={pokemon.name} clickable={true}>
+// 					<h1 className="text-xl text-center font-bold my-2">#{pokemon.number} - {capitalizeFirstLetter(pokemon.name)}</h1>
+// 					<Image src={pokemon.image} width={0} height={0} alt={pokemon.name} className="w-44 h-44"/>
+// 				</PokemonCard>
+// 			))}
+// 		</div>
+// 	)
+// }
