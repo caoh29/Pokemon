bb0530a50651c62d8b76097247e38c4b9b67dc37
Camilo Ordoñez <cronox20@gmail.com>
2024-01-09 09:34:29 -0500

    trying out ISR fetching directly inside app folder

--- src/app/page.js
+++ src/app/page.js
@@ -51,10 +51,17 @@
 // IF SOMETHING CHANGES IN THE DB IT IS NOT CHANGING IN THE SERVER, 
 // SOMEHOW WE NEED TO REVALIDATE THE DATA AFTER A CHANGE OCCURS IN THE DB BECAUSE THE USER COULD BE RECEIVING OUTDATED HTML
 
-import fetchPokemons from "@/api/pokeApi"
+//import fetchPokemons from "@/api/pokeApi"
 import PokemonCard from "@/components/PokemonCard/PokemonCard";
 import { capitalizeFirstLetter } from "@/lib/index";
 import Image from "next/image";
+
+async function fetchPokemons() {
+	// const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`);
+	const response = await fetch(`https://pokemon-backend-dg1j.onrender.com/api/pokemon`, { next: { revalidate: 60 } });
+	if (!response.ok) throw new Error('Failed to fetch pokemons. Check logs');
+	return response.json();
+}
 
 export default async function Home() {
 
